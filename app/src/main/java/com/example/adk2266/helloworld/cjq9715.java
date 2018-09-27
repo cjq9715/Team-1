00c76b316bc86eea25f6960898692b5838930a09
cjq9715 <43452067+cjq9715@users.noreply.github.com>
2018-09-27 15:45:27 -0400

    commit

---
+++ app/src/main/java/com/example/adk2266/helloworld/cjq9715.java
@@ -0,0 +1,13 @@
+package com.example.adk2266.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class cjq9715 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cjq9715);
+    }
+}
